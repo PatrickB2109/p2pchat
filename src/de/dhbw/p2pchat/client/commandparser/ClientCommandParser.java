d8a99b340350afe93f257e0a618e5f0e3ff5a5b9
lawand7 <lwand.allo@gmail.com>
2022-05-22 02:42:59 +0200

    first implementation of client2client connection

---
+++ src/de/dhbw/p2pchat/client/commandparser/ClientCommandParser.java
@@ -0,0 +1,12 @@
+package de.dhbw.p2pchat.client.commandparser;
+
+import de.dhbw.p2pchat.client.commands.*;
+
+public class ClientCommandParser extends UppercaseCommandParser {
+    private static final int HIERARCHY_LEVEL = 1;
+
+    public ClientCommandParser() {
+        super(HIERARCHY_LEVEL);
+        commands.put("CONNECT", new ConnectClientCommand());
+    }
+}
